e9d43e7bbdbc41f9d3b077e4dfff00f5977f9857
soparodi <soparodi@outlook.it>
2025-02-07 12:57:14 +0100

    Modifiche al 07-02-2025

--- dev2_2024/36_WebAppProduct/Pages/AggiungiProdotto.cshtml.cs
+++ dev2_2024/36_WebAppProduct/Pages/AggiungiProdotto.cshtml.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
 public class AggiungiProdottoModel : PageModel
--- dev2_2024/36_WebAppProduct/Pages/Prodotti.cshtml.cs
+++ dev2_2024/36_WebAppProduct/Pages/Prodotti.cshtml.cs
@@ -1,6 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
